60b67a24c81280c01c44a4f31fa39d6defb4b081
Muhasif Hafiz <muhasibhafeez91@gmail.com>
2024-11-04 04:16:50 +0530

    Retrieving Data in Firebase FireStore

--- app/src/main/java/com/muhasib/firebaselearning/FirestoreActivity.kt
+++ app/src/main/java/com/muhasib/firebaselearning/FirestoreActivity.kt
@@ -1,27 +1,31 @@
 package com.muhasib.firebaselearning
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.firestore.FirebaseFirestore
+import org.w3c.dom.Text
 
 class FirestoreActivity : AppCompatActivity() {
     private lateinit  var et_name :EditText
     private lateinit var et_add :EditText
     private lateinit var btn_add_data :Button
-    private lateinit var fireStore : FirebaseFirestore
+    private lateinit var nextPage : TextView
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_firestore)
 
+        nextPage=findViewById(R.id.firestoreDataActivity)
         et_add=findViewById(R.id.et_firestore_age)
         et_name=findViewById(R.id.et_firestore_name)
         btn_add_data=findViewById(R.id.btn_firestore_add)
@@ -40,6 +44,10 @@ class FirestoreActivity : AppCompatActivity() {
                     Toast.makeText(this@FirestoreActivity, "INSERTED", Toast.LENGTH_SHORT).show()
                 }
         }
+        nextPage.setOnClickListener {
+            startActivity(Intent(this@FirestoreActivity, DataListFirestoreActivity::class.java))
+        }
+
 
 
     }
